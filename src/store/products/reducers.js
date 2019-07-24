6f362c103b8f8245924d169d01e932fa427b9018
Donald Whitely <dswhitely1@gmail.com>
2019-07-24 12:21:29 -0400

    Installed Redux

---
+++ src/store/products/reducers.js
@@ -0,0 +1,32 @@
+import {createReducer} from "../../helpers/createReducer";
+import {REMOVE_FEATURE, ADD_FEATURE} from "./types";
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+        price: 26395,
+        name: '2019 Ford Mustang',
+        image:
+            'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+        features: []
+    },
+    store: [
+        { id: 1, name: 'V-6 engine', price: 1500 },
+        { id: 2, name: 'Racing detail package', price: 1500 },
+        { id: 3, name: 'Premium sound system', price: 500 },
+        { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+};
+
+const removeFeature = (state, payload) => {
+    console.log(`RemoveFeature`);
+};
+
+const addFeature = (state, payload) => {
+    console.log(`AddFeature`);
+};
+
+export default createReducer(initialState, {
+    [REMOVE_FEATURE]: removeFeature,
+    [ADD_FEATURE] : addFeature
+})
---
+++ src/store/products/types.js
@@ -0,0 +1,2 @@
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+export const ADD_FEATURE = 'ADD_FEATURE';
